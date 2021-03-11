d19fa7172bec799195a9f6bb462f4a3916690d0e
AK059 <75393105+AK059@users.noreply.github.com>
2021-03-11 13:11:16 +1300

    Add project files.

---
+++ Data/VisitorDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VisitorManagementSystem.Models;
+
+namespace VisitorManagementSystem.Data
+{
+    public class VisitorDbContext: DbContext
+    {
+        // Here we add in all the tables we are using.
+        public DbSet<StaffNames> StaffNames { get; set; }
+        public DbSet<Visitors> Visitor { get; set; }
+        // We need 2 constructors, one is empty, and the other injects in DbContextOptions
+        public VisitorDbContext(DbContextOptions<VisitorDbContext> options)
+        : base(options)
+        {
+        }
+        public VisitorDbContext()
+        {
+
+        }
+    }
+}
